766c0e52814158b5636fe6be9765a77ca7eb1e2c
Fred <Fred@MacBook-Pro-de-Fred.local>
2016-10-03 12:40:10 +0200

    Package JSON et premiers essais

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,47 @@
 
+import org.json.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+
+
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello there");
+		String apiKey = "0ad8c862866c0f99ff7ea5a58309fc13";
+		String url = "https://api.themoviedb.org/3/movie/286217?api_key="+apiKey;
+		JSONObject json = new JSONObject(callURL(url));
+		
+		System.out.println(json.toString());
+		
 	}
-
+ 
+	public static String callURL(String myURL) {
+		StringBuilder sb = new StringBuilder();
+		URLConnection urlConn = null;
+		InputStreamReader in = null;
+		try {
+			URL url = new URL(myURL);
+			urlConn = url.openConnection();
+			if (urlConn != null)
+				urlConn.setReadTimeout(60 * 1000);
+			if (urlConn != null && urlConn.getInputStream() != null) {
+				in = new InputStreamReader(urlConn.getInputStream(), Charset.defaultCharset());
+				BufferedReader bufferedReader = new BufferedReader(in);
+				if (bufferedReader != null) {
+					int cp;
+					while ((cp = bufferedReader.read()) != -1) {
+						sb.append((char) cp);
+					}
+					bufferedReader.close();
+				}
+			}
+			in.close();
+		} catch (Exception e) {
+			throw new RuntimeException("Exception while calling URL:" + myURL, e);
+		}
+ 
+		return sb.toString();
+	}
 }
