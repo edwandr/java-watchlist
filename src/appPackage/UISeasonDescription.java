e30314d6ac9cd4a0af719bd4d3055c0de985db4d
Elie Dutheil <eliedthl@gmail.com>
2016-10-25 17:57:02 +0200

    Ajout épisodes avec résumé au clic sur une saison

--- src/appPackage/UISeasonDescription.java
+++ src/appPackage/UISeasonDescription.java
@@ -4,6 +4,9 @@ import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+import java.util.Iterator;
 
 public class UISeasonDescription extends BorderPane implements Observer{
     public UISeasonDescription(){
@@ -12,6 +15,13 @@ public class UISeasonDescription extends BorderPane implements Observer{
     public void update (TVSeason season) {
         VBox description = new VBox();
         description.getChildren().addAll(new Label(season.getName()));
+
+        Iterator<TVEpisode> it = season.getEpisodes().iterator();
+
+        while (it.hasNext()){
+            TVEpisode episode = it.next();
+            description.getChildren().addAll(new Text(episode.getName()), new Text(episode.getOverview()));
+        }
         VBox.setMargin(description, new Insets(15, 0, 20, 20));
         this.setCenter(description);
     }
